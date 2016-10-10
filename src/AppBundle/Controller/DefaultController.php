03066f1fabf5f5d484ec3679892325b1c0555aba
pascual Muñoz <pascual.munoz@pccomponentes.com>
2016-10-10 22:43:29 +0200

    commit

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,12 +5,11 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use AppBundle\Entity\curso;
 
 class DefaultController extends Controller
 {
-    /**
-     * @Route("/", name="homepage")
-     */
+ 
     public function indexAction(Request $request)
     {
         // replace this example code with whatever you need
@@ -18,4 +17,16 @@ class DefaultController extends Controller
             'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
         ]);
     }
+       /**
+     * @Route("/", name="homepage")
+     */
+    public function addAction(Request $r) {
+        $c  = new Curso();
+        
+        
+    
+        
+        die();
+    }
+    
 }
