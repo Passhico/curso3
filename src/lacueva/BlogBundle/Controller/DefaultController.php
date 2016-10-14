6094f90fe460b8d45273e7783c14ce4d179916f5
passhico <passhico@tenopia>
2016-10-14 08:37:43 +0200

    usando el repo

--- src/lacueva/BlogBundle/Controller/DefaultController.php
+++ src/lacueva/BlogBundle/Controller/DefaultController.php
@@ -3,11 +3,25 @@
 namespace lacueva\BlogBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use lacueva\BlogBundle\Entity\Entries;
 
 class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('BlogBundle:Default:index.html.twig');
+
+		$em = $this->getDoctrine()->getManager();
+		$entry_repo = $em->getRepository('BlogBundle:Entries');
+		$entries = $entry_repo->findAll();
+		
+		$e = new \lacueva\BlogBundle\Entity\Entries;
+		foreach ($entries as $e)
+		{
+			echo $e->getTitle();
+		}
+		die();
+		
+		
+		 //       return $this->render('BlogBundle:Default:index.html.twig');
     }
 }
