e45ddfd38cdc6e82717676a5892e4691c60a1c53
passhico <passhico@tenopia>
2016-10-20 02:49:28 +0200

    CONFIGURANDO EL LOGIN .
    <?php
    
    namespace lacueva\BlogBundle\Entity;
    use \Symfony\Component\Security\Core\User\UserInterface;
    
    
    /**
     * Users
     */
    class Users implements \Symfony\Component\Security\Core\User\UserInterface
    {

---
+++ src/lacueva/BlogBundle/Controller/userController.php
@@ -0,0 +1,42 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace lacueva\BlogBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use \Symfony\Component\HttpFoundation\Request;
+
+use lacueva\BlogBundle\Entity\Users;
+
+
+class DefaultController extends Controller
+{
+    
+	
+   public function loginAction(\Request$r)    
+  {
+	   
+		$autenticationUtils = $this->get("security.authentication_utils");
+		$error = $autenticationUtils->getLastAuthenticationError(); 
+		$lastUsername = $autenticationUtils->getLastUsername();
+		
+		return $this->render("BlogBundle:login.html.twig", 
+					[	"error" => $error , 
+						"last_username" => $lastUsername, 
+												
+					]
+				)
+		
+				
+				
+				;
+		
+		
+		
+   } 
+}
