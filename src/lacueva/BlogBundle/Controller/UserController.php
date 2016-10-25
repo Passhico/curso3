490ff16f23d016bbe701aea692bd2925ea8902dc
Pascual <pascual.munoz@pccomponentes.com>
2016-10-25 13:25:26 +0200

    Añadido formulario para Resgistrarse en el blog.

--- src/lacueva/BlogBundle/Controller/UserController.php
+++ src/lacueva/BlogBundle/Controller/UserController.php
@@ -13,6 +13,8 @@ use \Symfony\Component\HttpFoundation\Request;
 
 use lacueva\BlogBundle\Entity\Users;
 
+use lacueva\BlogBundle\Form\UsersType;
+
 
 class UserController extends Controller
 {	
@@ -24,10 +26,33 @@ class UserController extends Controller
 		$lastUsername = $autenticationUtils->getLastUsername();
 		
 		
+		//El usuario a loguear . 
+		$user_to_log = new \lacueva\BlogBundle\Entity\Users();
+
+		//Creamos el form y le pasamos el curso par que lo cargue. 
+		$form = $this->createForm(\lacueva\BlogBundle\Form\UsersType::class , $user_to_log);
+		$form->handleRequest($r);
+
+		if ($form->isValid())
+		{
+			$status = "formulario válido" . NL;
+//			$data = [
+//				"titulo" => $form->get("titulo")->getData(),
+//				"descripcion" => $form->get("descripcion")->getData(),
+//				"precio" => $form->get("precio")->getData()
+//			];
+		} else
+		{
+			$status = "NO SE HA REGISTRADO CORRECTAMENTE";
+			$data = [];
+		}
+
+	
 		return $this->render("login.html.twig", 
 					[	"error" => $error , 
 						"last_username" => $lastUsername, 
-						"users" => $this->getDoctrine()->getRepository('BlogBundle:Users')->findAll()
+						"users" => $this->getDoctrine()->getRepository('BlogBundle:Users')->findAll(), 
+						"formulario" => $form->createView()
 					]
 				);
    } 
