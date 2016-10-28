fb827421d55b77d5b72dfbb31b850184d26b96b8
Pascual <pascual.munoz@pccomponentes.com>
2016-10-28 12:22:05 +0200

    ./console doctrine:generate:form BlogBundle:Tags

---
+++ src/lacueva/BlogBundle/Form/TagsType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace lacueva\BlogBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class TagsType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('name')->add('description')        ;
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'lacueva\BlogBundle\Entity\Tags'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'lacueva_blogbundle_tags';
+    }
+
+
+}
