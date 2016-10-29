038ddb66120901d83cfc1359fffca8780413f545
Pascual <pascual.munoz@pccomponentes.com>
2016-10-29 20:42:04 +0200

    8.72

--- src/lacueva/BlogBundle/Controller/TagController.php
+++ src/lacueva/BlogBundle/Controller/TagController.php
@@ -25,33 +25,34 @@ class TagController extends Controller
 	{
 		//rpeparamos una tag  (y le pasamos el EM ya que hay constraints que requieren de su uso ., 
 		// la restricción es "PreviamenteCreada"  y se imiplementa en el propio objeto como tiene que ser.. 
-		$tagToAdd = new \lacueva\BlogBundle\Entity\Tags($this->getDoctrine()->getManager());
+		$tagToPersist = new \lacueva\BlogBundle\Entity\Tags($this->getDoctrine()->getManager());
 
 		//Creamos el form 
-		$formAddTag = $this->createForm(\lacueva\BlogBundle\Form\TagsType::class, $tagToAdd);
-		//Le decimos que manejará la request cuando se pulse el botón submit... 
+		$formAddTag = $this->createForm(\lacueva\BlogBundle\Form\TagsType::class, $tagToPersist);
+		//Le decimos que manejará la request que le pasará el kernel al controlador. 
 		$formAddTag->handleRequest($request);
 
 		$status = "El form " . $formAddTag->getName() . " ";
 
-		if ($formAddTag->isSubmitted())
+		/*
+		 * Según : http://php.net/manual/es/language.operators.logical.php
+		 * Los if (usando un | ) se ejecutan de izda a derecha , y en cuanto hay un false deja de 
+		 * ejecutarse , esto , afortunadamente es como en C++
+		 * asi que no hace falta anidar IFs.
+		 */
+		if ($formAddTag->isSubmitted() || $formAddTag->isValid())
 		{
-			$this->_log("issubmited()");
-			if ($formAddTag->isValid())
-			{
 				$this->_log("isValid(true)");
 
 				//_formget 
-				$tagToAdd->setName($formAddTag->get("name")->getData());
-				$tagToAdd->setDescription($formAddTag->get("description")->getData());
-
-				// _persist	
-				$this->getDoctrine()->getManager()->persist($tagToAdd);
-				$this->getDoctrine()->getManager()->flush();
-			} else
-				$this->_log("isValid(false)");
-		} else
-			$this->_log("sin pulsar boton");
+				$tagToPersist->setName($formAddTag->get("name")->getData());
+				$tagToPersist->setDescription($formAddTag->get("description")->getData());
+				
+				// _persist
+				$this->getDoctrine()->getManager()->persist($tagToPersist);
+				if ($this->getDoctrine()->getManager()->flush())
+					$this->_log("La tag no se ha podido crear");
+		}
 
 		$this->_log("Y el nombre de la  flashbag es...: " . $this->_session->getFlashBag()->getName());
 		return $this->render("addTag.html.twig", [
@@ -64,5 +65,7 @@ class TagController extends Controller
 	{
 		$this->_session->getFlashBag()->add("status", $string);
 	}
+	
+
 
 }
