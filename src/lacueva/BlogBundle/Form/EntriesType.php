4138539f3c394f982d57e10d9a5febc4a384d919
Pascual <pascual.munoz@pccomponentes.com>
2016-10-31 11:10:33 +0100

    _builders correctos y enlazados excepto el de TAGS 8.75 (crear formulario entradas)

--- src/lacueva/BlogBundle/Form/EntriesType.php
+++ src/lacueva/BlogBundle/Form/EntriesType.php
@@ -10,7 +10,9 @@ use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 /*
  * Esto es lo importante aquí , con el campo EntitityType , podemos incluir dentro de un campo 
- * otro formulario correspondiente a una entidad.
+ * otro formulario correspondiente a una entidad. Esto hace una consulta a la entidad referenciada y 
+ * solictia un __tostring de la clase , así que este ToString tiene que ser implementado por defecto 
+ * en todas las Entidades.
  */
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 
@@ -22,35 +24,61 @@ class EntriesType extends AbstractType
 	 */
 	public function buildForm(FormBuilderInterface $builder, array $options)
 	{
-        $builder->add('title')->add('content')->add('status')->add('image')->add('idUser')->add('idCategory')        ;
-//		$builder->add('title', \Symfony\Component\Form\Extension\Core\Type\TextType::class, [
-//			"label" => "Titulo",
+//        $builder->add('title')->add('content')->add('status')->add('image')->add('idUser')->add('idCategory')        ;
+
+
+		$builder->add('idCategory', \Symfony\Bridge\Doctrine\Form\Type\EntityType::class, [
+			"class" => "BlogBundle:Categories", //pero de clase de objeto.
+			"label" => "Categoria",
+			"attr" =>
+			[
+				"class" => "form-control"
+			]
+		]);
+		$builder->add('idUser', \Symfony\Bridge\Doctrine\Form\Type\EntityType::class, [
+			"class" => "BlogBundle:Users", //pero de clase de objeto.
+			"label" => "Usuario",
+			"attr" =>
+			[
+				"class" => "form-control"
+			]
+		]);
+//		$builder->add('idTag', \Symfony\Bridge\Doctrine\Form\Type\EntityType::class, [
+//			"class" => "BlogBundle:Tags", //pero de clase de objeto.
+//			"label" => "Tags",
 //			"attr" =>
 //			[
 //				"class" => "form-control"
 //			]
 //		]);
-//		$builder->add('content', \Symfony\Component\Form\Extension\Core\Type\TextareaType::class, [
-//			"label" => "Contenido",
-//			"attr" =>
-//			[
-//				"class" => "form-control"
-//			]
-//		]);
-//		$builder->add('status', \Symfony\Component\Form\Extension\Core\Type\ChoiceType::class, [
-//			"label" => "status",
-//			"choices" => ["publicado" => "publicado", "privado" => "privado"],
-//			"attr" => ["class" => "form-control"]
-//		]);
-//		$builder->add('idCategory', \Symfony\Bridge\Doctrine\Form\Type\EntityType::class, [
-//			"class" => "BlogBundle:Categories", //pero de clase de objeto.
-//			"label" => "Categoria",
-//			"attr" =>
-//			[
-//				"class" => "form-control"
-//			]
-//		]);
-		
+
+		$builder->add('title', \Symfony\Component\Form\Extension\Core\Type\TextType::class, [
+			"label" => "Titulo",
+			"attr" =>
+			[
+				"class" => "form-control"
+			]
+		]);
+		$builder->add('content', \Symfony\Component\Form\Extension\Core\Type\TextareaType::class, [
+			"label" => "Contenido",
+			"attr" =>
+			[
+				"class" => "form-control"
+			]
+		]);
+		$builder->add('status', \Symfony\Component\Form\Extension\Core\Type\ChoiceType::class, [
+			"label" => "status",
+			"choices" => ["Público" => "public", "Privado" => "private"],
+			"attr" => ["class" => "form-control"]
+		]);
+
+		$builder->add('botonGuardar', \Symfony\Component\Form\Extension\Core\Type\SubmitType::class, [
+			"label" => "guardar",
+			"attr" =>
+			[
+				"class" => "btn btn-success form-control"
+			]
+		]);
 //		$builder->add('tags', \Symfony\Component\Form\Extension\Core\Type\TextType::class, 		[
 //
 //			"label" => "tags", 
