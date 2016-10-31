95204932f26442213956157db013a3275a681a83
Pascual <pascual.munoz@pccomponentes.com>
2016-10-31 18:20:39 +0100

    8.75 (crear formulario entradas) Se obtiene el user de la sessión para que la entrada se genere con el nombre de quien la crea.

--- src/lacueva/BlogBundle/Controller/EntriesController.php
+++ src/lacueva/BlogBundle/Controller/EntriesController.php
@@ -34,22 +34,45 @@ class EntriesController extends Controller
 
 		$formularioEntrada = $this->createForm(\lacueva\BlogBundle\Form\EntriesType::class, $entradaToAdd);
 		$formularioEntrada->handleRequest($request);
-		
+
 
 		if ($formularioEntrada->isSubmitted())
 		{
 			if (( $formularioEntrada->isValid()))
 			{
-				/*
+				/* EXPLICACIÓN IMPORTANTE: 
 				 *  Aquí no tenemos que hacer nada más , ni geters ni seters 
 				 * Porque directamente , al crear el formulario hemos bindeado
 				 * la entidad , y esto hace que cuando se hace el flush() se sincroniza automáticamente
+				 * Por supuesto , sí que tendremos que modificar algunos valores tal cual 
+				 * se quedan cargados en la entidad si no es el valor que queremos . 
+				 * 
+				 * Un ejemplo es la img , en la que se crea el nombre temporal y es el que 
+				 * quedaría en la base de datos si hacemos el flush antes de copiar el archivo
+				 * y cambiarla el nombre, otra cosa que hay que dejar cargada , sería el id del 
+				 * usuario , el cual no debe poder elegirse desde el formulario , sino que 
+				 * ha de hacerse de forma automática. 
 				 * 
 				 */
 				
 				
+				
+				/*
+				 * TODO: LA IMAGEN
+				 */
+				$imageUploaded = new \Symfony\Component\HttpFoundation\File\UploadedFile(
+						$formularioEntrada['image']->getData(), //ruta
+						'foto_entrada_' . $entradaToAdd->getIdUser() . $entradaToAdd->getTitle() //nombre
+				);
+				$imageUploaded->move("images", 'foto_entrada_' . $entradaToAdd->getIdUser() . $entradaToAdd->getTitle());
+				$entradaToAdd->setImage($imageUploaded);
+				
+				/************************************************/
+				
+				//_getuser
+				$entradaToAdd->setIdUser($this->getUser());
 
-	
+				//persist
 				$this->getDoctrine()->getManager()->persist($entradaToAdd);
 				if ($this->getDoctrine()->getManager()->flush())
 					$this->_log("No se ha podido insertar la Entrada ");
@@ -62,11 +85,9 @@ class EntriesController extends Controller
 		]);
 	}
 
-	
 	//PRIVS
 	private function _log($dumpeame)
 	{
-
 
 
 		$this->_session->getFlashBag()->add("log", $string);
