63cf2d3cfc28ab00851c44ceb4df4e35bae8e811
Pascual Muñoz Galián <pascual.munoz@pccomponentes.com>
2016-11-13 19:51:50 +0100

    Añadid ApiGator para acceso a API rest con Curl de EXPRESS51 en Controlador Symfony

--- src/lacueva/BlogBundle/Controller/Express51ConversationsController.php
+++ src/lacueva/BlogBundle/Controller/Express51ConversationsController.php
@@ -1,13 +1,15 @@
 <?php
 namespace lacueva\BlogBundle\Controller; // vg->lacueva\BlogBundle\Controller (el completion works);
 include 'ApiGator.php';
+
 use ApiGator\ApiGator;
-
-
+use Closure;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Session\Session;
+use const SECTION_CONVERSACIONES;
+use const SECTION_STATUS;
 use function dump;
 
 
@@ -39,24 +41,41 @@ class Express51ConversationsController extends Controller {
 		 */
 			
 		
-		$Express51ConversationsApi = new \ApiGator\ApiGator(SECTION_STATUS);
-		
-		
-		/* @var $closureGeneraArray Closure */
-		$closureGeneraArray = function( $json )
+		/* @var $funcionDumpComoArray Closure */
+		$funcionDumpComoArray = function( $json )
 		{
-			
+			$arr = json_decode($json, true);
+			return new Response (dump($arr));
 		};
-			
-		$Express51ConversationsApi->procesaResponseCon($closureGeneraArray);
+	
+		//status
+		$ApiGatorExpress51_STATUS = new \ApiGator\ApiGator(SECTION_STATUS);
+		$ApiGatorExpress51_STATUS->procesaResponseCon($funcionDumpComoArray);
+	
+		//conversations
+		$ApiGatorExpress51_CONVERSATIONS = new ApiGator(SECTION_CONVERSACIONES);
+		$ApiGatorExpress51_CONVERSATIONS->procesaResponseCon($funcionDumpComoArray);
+		
+		
+		//customers
+		$ApiGatorExpress51_CUSTOMERS = new ApiGator('customers');
+		$ApiGatorExpress51_CUSTOMERS->procesaResponseCon($funcionDumpComoArray);
+		
+		
+	
+		
+		
+		
+//			curl_setopt($ch,CURLOPT_URL,"http://example.com/api/reports/agents?date_from=[value]&date_to=[value]?&date_from=value&date_to=value&apikey=value");
 
 
-
-		//_render aqui. 
-		;
-		return new Response(dump($Express51ConversationsApi->curl_response) .
-				"Esto es una Stub de indexAction, posiblemente quieras usar en esta linea :
-		  return \$this->render(\$view)");
+		
+	
+		return new Response('Datos de la API de Express51');
+		
+		
+		
+		
 	}
 
 	//PRIVS
