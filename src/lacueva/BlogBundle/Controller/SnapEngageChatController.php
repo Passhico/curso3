5b5d21b01d67f053a762d7f3cfc1d9c4af79153c
Pascual Muñoz Galián <pascual.munoz@pccomponentes.com>
2016-11-17 04:30:21 +0100

    parche CreateEntitiesCases()

--- src/lacueva/BlogBundle/Controller/SnapEngageChatController.php
+++ src/lacueva/BlogBundle/Controller/SnapEngageChatController.php
@@ -26,7 +26,7 @@ define('SNAPCHAT_ORG_ID', '6418107096367104');
 define('SNAPCHAT_APITOKEN', 'ebec63f521baf484da13a550a111e5d6');
 define('SNAPCHAT_WIDGET_ID', '4e09afaa-f6c5-4d73-9fae-5b85b0e4aee6');
 
-define('SNAPCHAT_URI', SNAPCHAT_URL . SNAPCHAT_ORG_ID . '/logs?widgetId=' . SNAPCHAT_WIDGET_ID . '&start=2016-11-01&end=2016-11-15');
+define('SNAPCHAT_URI', SNAPCHAT_URL . SNAPCHAT_ORG_ID . '/logs?widgetId=' . SNAPCHAT_WIDGET_ID . '&start=2016-11-01&end=2016-11-17');
 /*
  * Crea conexiones a la api de datos de SnapChat para PcComponentes.com
  * 
@@ -159,6 +159,11 @@ class SnapEngageChatController extends Controller {
 				$caseToAdd->setLanguageCode($case['language_code']);
 				$caseToAdd->setTranscripts($case['transcripts']);
 				$caseToAdd->setJavascriptVariables($case['javascript_variables']);
+				
+				//	var_dump(array_keys($case));
+			dump($caseToAdd);
+			$this->counterCasesToAdd = $this->counterCasesToAdd + 1;
+
 			}
 		} catch (Exception $exc) {
 			/* @var $exc ContextErrorException */
@@ -167,10 +172,7 @@ class SnapEngageChatController extends Controller {
 		} finally {
 
 
-			//	var_dump(array_keys($case));
-			dump($caseToAdd);
-			$this->counterCasesToAdd = $this->counterCasesToAdd + 1;
-
+			
 			// _persist
 
 
