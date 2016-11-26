e78e746148aa8f6f7a6ce1e3d0b2d41189b0cf17
Pascual <pascual.munoz@pccomponentes.com>
2016-11-26 01:01:44 +0100

    quitadas variables para los repos del __construct

--- src/lacueva/BlogBundle/Controller/ConsultasDelChat.sql
+++ src/lacueva/BlogBundle/Controller/ConsultasDelChat.sql
@@ -1,10 +1,6 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 /**
- * Author:  passhico
+ * Author:  Pascual Muñoz 
  * Created: 24-nov-2016
  */
 
